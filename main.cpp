b9a5d3b87728a5f37c808e4ffdea7ee7e54b2ecf
d4nielxo <dhaddad3@students.avc.edu>
2026-04-13 13:45:30 -0700

    commit 1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main() 
 {
-    int N, M;
+    int n, m;
+    int result = 1;
 
     cout << "Enter your base number: ";
-    cin >> N;
+    cin >> n;
+
     cout << "Enter your exponent: ";
-    cin >> M;
+    cin >> m;
 
-    cout << N << " to power number: ";
+    cout << n << " to power number: ";
 
-    // TODO
-
-    cout << endl;
+    for (int i = 1; i <= m; i++) {
+        result = result * n;
+        cout << result;
+        if (i < m) {
+            cout << ", ";
+        }
+    }
 
     return 0;
 }
